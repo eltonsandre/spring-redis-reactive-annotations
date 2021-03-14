37f4dd46091a968d66f774b93205a9d2456df20b
Elton S. André <dbc.elton@agibank.com.br>
2021-03-14 03:44:08 -0300

    annotation with reactive support

---
+++ src/main/java/com/gtihub/eltonsandre/spring/reactive/redis/SpringRedisReactiveAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.gtihub.eltonsandre.spring.reactive.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisReactiveAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisReactiveAnnotationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtihub/eltonsandre/spring/reactive/redis/SpringRedisReactiveAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtihub.eltonsandre.spring.reactive.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisReactiveAnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
